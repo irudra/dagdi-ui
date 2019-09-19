9d3fb8e6e26e6ec95dccb751ee95a8f9339e9743
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-09-19 10:26:16 +0530

    Front page

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss";
 
-import Index from "views/Index.jsx";
+import Index from "views/Home/home.jsx";
 import Landing from "views/dagdi/Landing.jsx";
 import Login from "views/pages/Login.jsx";
 import Profile from "views/pages/Profile.jsx";
