7cf6f23e6ebaf6786b0457768263a8592c29d4f5
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-09-20 17:52:52 +0530

    #5 added search detail page

--- src/views/pages/Register.jsx
+++ src/views/pages/Register.jsx
@@ -70,36 +70,36 @@ class Register extends React.Component {
                                             <div className="text-muted text-center mb-3">
                                                 <small>Sign up with</small>
                                             </div>
-                                            <div className="text-center">
-                                                <Button
-                                                    className="btn-neutral btn-icon mr-4"
-                                                    color="default"
-                                                    href="#pablo"
-                                                    onClick={e => e.preventDefault()}
-                                                >
+                                            <div className="btn-wrapper text-center">
+                        <Button
+                          className="btn-neutral btn-icon"
+                          color="default"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
                           <span className="btn-inner--icon mr-1">
                             <img
-                                alt="..."
-                                src={require("assets/img/icons/common/github.svg")}
+                              alt="..."
+                              src={require("assets/img/icons/common/github.svg")}
                             />
                           </span>
-                                                    <span className="btn-inner--text">Github</span>
-                                                </Button>
-                                                <Button
-                                                    className="btn-neutral btn-icon ml-1"
-                                                    color="default"
-                                                    href="#pablo"
-                                                    onClick={e => e.preventDefault()}
-                                                >
+                          <span className="btn-inner--text">Github</span>
+                        </Button>
+                        <Button
+                          className="btn-neutral btn-icon ml-1"
+                          color="default"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
                           <span className="btn-inner--icon mr-1">
                             <img
-                                alt="..."
-                                src={require("assets/img/icons/common/google.svg")}
+                              alt="..."
+                              src={require("assets/img/icons/common/google.svg")}
                             />
                           </span>
-                                                    <span className="btn-inner--text">Google</span>
-                                                </Button>
-                                            </div>
+                          <span className="btn-inner--text">Google</span>
+                        </Button>
+                      </div>
                                         </CardHeader>
                                         <CardBody className="px-lg-5 py-lg-5">
                                             <div className="text-center text-muted mb-4">
