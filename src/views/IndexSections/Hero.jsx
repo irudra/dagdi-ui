5c37c9fba16100d2f13487a0987397971d43bd6e
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-09-23 13:42:00 +0530

    Table component created

--- src/views/IndexSections/Hero.jsx
+++ src/views/IndexSections/Hero.jsx
@@ -19,7 +19,7 @@ import React from "react";
 
 // reactstrap components
 import { Container } from "reactstrap";
-import { Link } from "react-router-dom";
+  import { Link } from "react-router-dom";
 
 class Hero extends React.Component {
   render() {
