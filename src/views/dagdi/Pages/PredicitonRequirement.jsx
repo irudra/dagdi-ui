8129835f9de3a4f4733be8dd3c69e15f36fb6941
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-09-26 15:45:41 +0530

    user preferences details added in the prediction page

--- src/views/dagdi/Pages/PredicitonRequirement.jsx
+++ src/views/dagdi/Pages/PredicitonRequirement.jsx
@@ -12,6 +12,7 @@ import {
 import Marketing from "views/dagdi/Marketing.jsx";
 import PredictionCarousel from "views/dagdi/prediction/PredictionCarousel.jsx";
 import TerrainDetails from "views/dagdi/prediction/TerrainDetails.jsx";
+import Preferences from "views/dagdi/prediction/Preferences.jsx";
 
 var steps = [
   // this step hasn't got a isValidated() function, so it will be considered to be true
@@ -19,7 +20,7 @@ var steps = [
 
   { stepName: "First", component: TerrainDetails },
   // this step will be validated to false
-  { stepName: "Second", component: Marketing },
+  { stepName: "Second", component: Preferences },
   // this step will never be reachable because of the seconds isValidated() steps function that will always return false
   // tis is test
   { stepName: "Third", component: Marketing },
