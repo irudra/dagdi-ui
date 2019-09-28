1f77971150296487a71aac15ed6b5bc0c3a5c678
rudra <hemant.gaur11@gmail.com>
2019-09-28 10:22:42 +0530

    #24 : Added language support for the website to follow.
    Added language support for the website to follow.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss";
-
+import {I18nextProvider} from "react-i18next";
 import Index from "views/Home/home.jsx";
 import Landing from "views/dagdi/Pages/Landing.jsx";
 import Login from "views/pages/Login.jsx";
@@ -14,40 +13,42 @@ import Profile from "views/pages/Profile.jsx";
 import Register from "views/pages/Register.jsx";
 import Prediction from "views/dagdi/Pages/Prediction.jsx";
 import PredictionRequirement from "views/dagdi/Pages/PredicitonRequirement.jsx";
+import i18n from "./i18n";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact render={props => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        exact
-        render={props => <Landing {...props} />}
-      />
-      <Route path="/" exact render={props => <Index {...props} />} />
-      <Route
-        path="/prediction-page"
-        exact
-        render={props => <Prediction {...props} />}
-      />
-      <Route
-        path="/prediction-requirement"
-        exact
-        render={props => <PredictionRequirement {...props} />}
-      />
-      <Route path="/login-page" exact render={props => <Login {...props} />} />
-      <Route
-        path="/profile-page"
-        exact
-        render={props => <Profile {...props} />}
-      />
-      <Route
-        path="/register-page"
-        exact
-        render={props => <Register {...props} />}
-      />
-      <Redirect to="/" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <I18nextProvider i18n={i18n}>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact render={props => <Index {...props} />}/>
+                <Route
+                    path="/landing-page"
+                    exact
+                    render={props => <Landing {...props} />}
+                />
+                <Route path="/" exact render={props => <Index {...props} />}/>
+                <Route
+                    path="/prediction-page"
+                    exact
+                    render={props => <Prediction {...props} />}
+                />
+                <Route
+                    path="/prediction-requirement"
+                    exact
+                    render={props => <PredictionRequirement {...props} />}
+                />
+                <Route path="/login-page" exact render={props => <Login {...props} />}/>
+                <Route
+                    path="/profile-page"
+                    exact
+                    render={props => <Profile {...props} />}
+                />
+                <Route
+                    path="/register-page"
+                    exact
+                    render={props => <Register {...props} />}
+                />
+                <Redirect to="/"/>
+            </Switch>
+        </BrowserRouter></I18nextProvider>,
+    document.getElementById("root")
 );
