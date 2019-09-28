ffa35a540319616033d8ef24325c6f1688cbbcbe
rudra <hemant.gaur11@gmail.com>
2019-09-28 13:00:26 +0530

    #24 : Moved language translation strings to separate files.
    Moved language translation strings to separate files.

--- src/i18n.js
+++ src/i18n.js
@@ -1,68 +1,15 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-
+import translationEng from "locales/en/translation.json";
+import translationHindi from "locales/hin/translation";
 i18n.use(LanguageDetector).init({
     // we init with resources
     resources: {
         hin: {
-            translations: {
-                Introduction: "प्रस्तावना",
-                "is an internationalization-framework which offers a complete solution to localize your product from web to mobile and desktop":
-                    "एक अंतर्राष्ट्रीयकरण - ढांचा है जो आपके उत्पाद को वेब से मोबाइल और डेस्कटॉप पर स्थानांतरित करने का एक संपूर्ण समाधान प्रदान करता है",
-                "Plugins to detect the user language":
-                    "उपयोगकर्ता भाषा का पता लगाने के लिए प्लगइन्स",
-                "Plugins to load translations": "अनुवाद लोड करने के लिए प्लगइन्स",
-                "Optionally cache the translations": "वैकल्पिक रूप से अनुवाद कैश करें",
-                Advantages: "लाभ",
-                "Flexibility to use other packages":
-                    "अन्य पैकेजों का उपयोग करने के लिए लचीलापन"
-            }
+            translations: translationHindi
         },
         en: {
-            translations: {
-                Introduction: "Introduction sdasfas",
-                "is an internationalization-framework which offers a complete solution to localize your product from web to mobile and desktop":
-                    "is an internationalization-framework which offers a complete solution to localize your product from web to mobile and desktop",
-                "Plugins to detect the user language":
-                    "Plugins to detect the user language",
-                "Plugins to load translations": "Plugins to load translations",
-                "Optionally cache the translations":
-                    "Optionally cache the translations",
-                Advantages: "Advantages",
-                "hero.DagdiSystem": "An intelligent System to solve day-to-day life problems of Rural Areas",
-                "Flexibility to use other packages": "Flexibility to use other packages"
-            }
-        },
-        jap: {
-            translations: {
-                Introduction: "前書き",
-                "is an internationalization-framework which offers a complete solution to localize your product from web to mobile and desktop":
-                    "Webからモバイルとデスクトップに製品をローカライズするための完全なソリューションを提供する国際化フレームワークです",
-                "Plugins to detect the user language":
-                    "ユーザー言語を検出するためのプラグイン",
-                "Plugins to load translations": "翻訳をロードするためのプラグイン",
-                "Optionally cache the translations": "必要に応じて翻訳をキャッシュする",
-                Advantages: "利点",
-                "Flexibility to use other packages": "他のパッケージを使用する柔軟性"
-            }
-        },
-
-
-
-        ger: {
-            translations: {
-                Introduction: "Einführung",
-                "is an internationalization-framework which offers a complete solution to localize your product from web to mobile and desktop":
-                    "ist ein Internationalisierungs-Framework, das eine Komplettlösung für die Lokalisierung Ihres Produkts vom Web auf das Handy und den Desktop bietet",
-                "Plugins to detect the user language":
-                    "Plugins zur Erkennung der Benutzersprache",
-                "Plugins to load translations": "Plugins zum Laden von Übersetzungen",
-                "Optionally cache the translations":
-                    "Optional die Übersetzungen zwischenspeichern",
-                Advantages: "Vorteile",
-                "Flexibility to use other packages":
-                    "Flexibilität zur Verwendung anderer Pakete"
-            }
+            translations: translationEng
         },
         fre: {
             translations: {
@@ -80,7 +27,7 @@ i18n.use(LanguageDetector).init({
             }
         }
     },
-    fallbackLng: "hin",
+    fallbackLng: "en",
     debug: true,
 
     // have a common namespace used around the full app
