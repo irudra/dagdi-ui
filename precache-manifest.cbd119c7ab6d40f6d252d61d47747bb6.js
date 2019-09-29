5360a9ee1355dd316dfeaa33ddafbc9b06b1405d
rudra <hemant.gaur11@gmail.com>
2019-09-29 18:00:11 +0530

    Updates

---
+++ precache-manifest.cbd119c7ab6d40f6d252d61d47747bb6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e977109c5c4ec0032a69554017e79d25",
+    "url": "/irudra/index.html"
+  },
+  {
+    "revision": "7ee4dc38c5bec70caeb5",
+    "url": "/irudra/static/css/2.83c65ca0.chunk.css"
+  },
+  {
+    "revision": "d4aee6aa195142ca6e40",
+    "url": "/irudra/static/css/main.d6b50bf3.chunk.css"
+  },
+  {
+    "revision": "7ee4dc38c5bec70caeb5",
+    "url": "/irudra/static/js/2.047b7a96.chunk.js"
+  },
+  {
+    "revision": "d4aee6aa195142ca6e40",
+    "url": "/irudra/static/js/main.88e81c92.chunk.js"
+  },
+  {
+    "revision": "2ec9a6f9cfa5ab41c3b7",
+    "url": "/irudra/static/js/runtime~main.e07db470.js"
+  },
+  {
+    "revision": "30d2c6710896e433060716bc00e30d2c",
+    "url": "/irudra/static/media/Nigam.30d2c671.jpg"
+  },
+  {
+    "revision": "11ff20c63b488409cc9a023803f835cf",
+    "url": "/irudra/static/media/appbuddylogo.11ff20c6.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/irudra/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "7a3205893fc277aafe81e22568820ced",
+    "url": "/irudra/static/media/dagdi_logo.7a320589.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/irudra/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/irudra/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/irudra/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/irudra/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/irudra/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/irudra/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/irudra/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "d02dac52af9a0436946a7eedd8022401",
+    "url": "/irudra/static/media/hemant.d02dac52.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/irudra/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/irudra/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "ba449a0697388668ef05e043ffdeafca",
+    "url": "/irudra/static/media/img-2-1200x1000.ba449a06.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/irudra/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/irudra/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/irudra/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/irudra/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/irudra/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/irudra/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/irudra/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/irudra/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/irudra/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/irudra/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
