2fe38d602efe463d7342a9d19a7153cd7435e786
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-10-01 16:36:07 +0530

    #31 Our team and contact  us moved to language

--- src/views/dagdi/ContactUs.jsx
+++ src/views/dagdi/ContactUs.jsx
@@ -1,85 +1,102 @@
-import React from "react";
+
  import {Container,FormGroup,Col,Row,Card,InputGroup,Input,Button,CardBody,InputGroupAddon,InputGroupText} from 'reactstrap';
  import classnames from 'classnames';
+ import { useTranslation } from "react-i18next";
+ import React, { useState, useEffect } from 'react'
+
+ 
+function ContactFunction() {
+ 
+    const initialUserState = {
+        state: {},
+        loading: true,
+    }
+
+    const [state, setState] = useState(initialUserState)
+    const { t } = useTranslation();
+    return <section className="section section-lg pt-lg-0 section-contact-us">
+        <Container>
+            <Row className="justify-content-center mt--300">
+                <Col lg="8">
+                    <Card className="bg-gradient-secondary shadow">
+                        <CardBody className="p-lg-5">
+                            <h4 className="mb-1">{t("Contact Us")}</h4>
+                            <p className="mt-0">
+                            {t("Contact Us descriiption")}
+                            </p>
+                            <FormGroup
+                                className={classnames("mt-5", {
+                                    focused: state.nameFocused
+                                })}
+                            >
+                                <InputGroup className="input-group-alternative">
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText>
+                                            <i className="ni ni-user-run"/>
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input
+                                        placeholder={t("Your name")}
+                                        type="text"
+                                        onFocus={e => setState({nameFocused: true})}
+                                        onBlur={e => setState({nameFocused: false})}
+                                    />
+                                </InputGroup>
+                            </FormGroup>
+                            <FormGroup
+                                className={classnames({
+                                    focused: state.emailFocused
+                                })}
+                            >
+                                <InputGroup className="input-group-alternative">
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText>
+                                            <i className="ni ni-email-83"/>
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input
+                                        placeholder={t("Email")}
+                                        type="email"
+                                        onFocus={e => this.setState({emailFocused: true})}
+                                        onBlur={e => this.setState({emailFocused: false})}
+                                    />
+                                </InputGroup>
+                            </FormGroup>
+                            <FormGroup className="mb-4">
+                                <Input
+                                    className="form-control-alternative"
+                                    cols="80"
+                                    name="name"
+                                    placeholder={t("message")}
+                                    rows="4"
+                                    type="textarea"
+                                />
+                            </FormGroup>
+                            <div>
+                                <Button
+                                    block
+                                    className="btn-round"
+                                    color="default"
+                                    size="lg"
+                                    type="button"
+                                >
+                                    {t("send message")}
+                                </Button>
+                            </div>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+
+}
+
 class ContactUs extends React.Component{
-    state={};
+    
     render(){
         return(
-            <section className="section section-lg pt-lg-0 section-contact-us">
-            <Container>
-                <Row className="justify-content-center mt--300">
-                    <Col lg="8">
-                        <Card className="bg-gradient-secondary shadow">
-                            <CardBody className="p-lg-5">
-                                <h4 className="mb-1">Want to work with us?</h4>
-                                <p className="mt-0">
-                                    Your project is very important to us.
-                                </p>
-                                <FormGroup
-                                    className={classnames("mt-5", {
-                                        focused: this.state.nameFocused
-                                    })}
-                                >
-                                    <InputGroup className="input-group-alternative">
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <i className="ni ni-user-run"/>
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input
-                                            placeholder="Your name"
-                                            type="text"
-                                            onFocus={e => this.setState({nameFocused: true})}
-                                            onBlur={e => this.setState({nameFocused: false})}
-                                        />
-                                    </InputGroup>
-                                </FormGroup>
-                                <FormGroup
-                                    className={classnames({
-                                        focused: this.state.emailFocused
-                                    })}
-                                >
-                                    <InputGroup className="input-group-alternative">
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <i className="ni ni-email-83"/>
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input
-                                            placeholder="Email address"
-                                            type="email"
-                                            onFocus={e => this.setState({emailFocused: true})}
-                                            onBlur={e => this.setState({emailFocused: false})}
-                                        />
-                                    </InputGroup>
-                                </FormGroup>
-                                <FormGroup className="mb-4">
-                                    <Input
-                                        className="form-control-alternative"
-                                        cols="80"
-                                        name="name"
-                                        placeholder="Type a message..."
-                                        rows="4"
-                                        type="textarea"
-                                    />
-                                </FormGroup>
-                                <div>
-                                    <Button
-                                        block
-                                        className="btn-round"
-                                        color="default"
-                                        size="lg"
-                                        type="button"
-                                    >
-                                        Send Message
-                                    </Button>
-                                </div>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
+       <ContactFunction/>
         );
     }
 }
