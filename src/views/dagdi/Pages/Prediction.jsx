53b5ac858d3e6b5e4903715ff39133fb757372ea
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-10-02 18:29:19 +0530

    #31 wizard details has been moved to language file

--- src/views/dagdi/Pages/Prediction.jsx
+++ src/views/dagdi/Pages/Prediction.jsx
@@ -1,7 +1,121 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 // reactstrap components
 import { Card, CardFooter, CardHeader, Container, Pagination, PaginationItem, PaginationLink, Row, Table } from "reactstrap";
+function PredictionFucntion() {
+  const { t } = useTranslation();
+  return <Container className="mb-xl" fluid>
+  {/* Table */}
+  <Row>
+    <div className="col">
+      <Card className="shadow">
+        <CardHeader className="border-0 text-center">
+          <h3 className="mb-0 ">{t("Card tables")}</h3>
+        </CardHeader>
+        <Table className="align-items-center table-flush" responsive>
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">{t("Project")}</th>
+              <th class="text-center" scope="col">{t("Budget")}</th>
+              <th scope="col">{t("Status")}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="w-25">
+                <img
+                  alt="tomato"
+                  className="img-fluid img-thumbnail"
+                  src={require("assets/img/theme/CorpTomato.jpg")}
+                />
+              </td>
+              <td>
+                {t("table row1 description")}
+    </td>
+              <td>
+                <tr><Link to='/corps-page/irudra'><button type="button" class="btn btn-primary">Details</button></Link></tr>
+                <tr> <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button></tr>
+                <tr> <button type="button" class="btn btn-primary"><i class="fas fa-edit"></i></button></tr>
+                <tr> <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button></tr>
+              </td>
+            </tr>
+            <tr>
+              <td class="w-25">
+                <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg" class="img-fluid img-thumbnail" alt="Sheep" />
+              </td>
+              <td>{t("table row2 description")}</td>
+              <td>Cristina</td>
+            </tr>
+            <tr>
+              <td class="w-25">
+                <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg" class="img-fluid img-thumbnail" alt="Sheep" />
+              </td>
+              <td>{t("table row3 description")}</td>
+              <td>Cristina</td>
+            </tr>
+          </tbody>
+        </Table>
+        <CardFooter className="py-4">
+          <nav aria-label="...">
+            <Pagination
+              className="pagination justify-content-end mb-0"
+              listClassName="justify-content-end mb-0"
+            >
+              <PaginationItem className="disabled">
+                <PaginationLink
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                  tabIndex="-1"
+                >
+                  <i className="fas fa-angle-left" />
+                  <span className="sr-only">Previous</span>
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem className="active">
+                <PaginationLink
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  1
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  2 <span className="sr-only">(current)</span>
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  3
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <i className="fas fa-angle-right" />
+                  <span className="sr-only">{t("Next")}</span>
+                </PaginationLink>
+              </PaginationItem>
+            </Pagination>
+          </nav>
+        </CardFooter>
+      </Card>
+    </div>
+  </Row>
+
+</Container>
+  }
+
+
 class Prediction extends React.Component {
   state = {
     name: "name"
@@ -10,128 +124,7 @@ class Prediction extends React.Component {
   render() {
     return (
       <>
-        <Container className="mb-xl" fluid>
-          {/* Table */}
-          <Row>
-            <div className="col">
-              <Card className="shadow">
-                <CardHeader className="border-0 text-center">
-                  <h3 className="mb-0 ">Card tables</h3>
-                </CardHeader>
-                <Table className="align-items-center table-flush" responsive>
-                  <thead className="thead-light">
-                    <tr>
-                      <th scope="col">Project</th>
-                      <th class="text-center" scope="col">Budget</th>
-                      <th scope="col">Status</th>
-
-
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td class="w-25">
-                        <img
-                          alt="tomato"
-                          className="img-fluid img-thumbnail"
-                          src={require("assets/img/theme/CorpTomato.jpg")}
-                        />
-
-                      </td>
-                      <td>Domestic investor wealth so ared by Rs 2.11 lakh crore in morning trade on Friday as equity market rallied following a slew of
-                        economy-boosting announcements by finance minister Nirmala Sitharaman. The BSE Sensex advanced over 1,300 points to a high of 37,421.70.
-            </td>
-                      <td>
-                        <tr><Link to='/corps-page/irudra'><button type="button" class="btn btn-primary">Details</button></Link></tr>
-                        <tr> <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button></tr>
-                        <tr> <button type="button" class="btn btn-primary"><i class="fas fa-edit"></i></button></tr>
-                        <tr> <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button></tr>
-                      </td>
-
-                    </tr>
-
-                    <tr>
-
-                      <td class="w-25">
-                        <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg" class="img-fluid img-thumbnail" alt="Sheep" />
-                      </td>
-                      <td>Bootstrap Grid 4 Tutorial and Examples</td>
-                      <td>Cristina</td>
-
-
-                    </tr>
-
-                    <tr>
-
-                      <td class="w-25">
-                        <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg" class="img-fluid img-thumbnail" alt="Sheep" />
-                      </td>
-                      <td>Bootstrap Grid 4 Tutorial and Examples</td>
-                      <td>Cristina</td>
-
-
-                    </tr>
-
-
-                  </tbody>
-                </Table>
-                <CardFooter className="py-4">
-                  <nav aria-label="...">
-                    <Pagination
-                      className="pagination justify-content-end mb-0"
-                      listClassName="justify-content-end mb-0"
-                    >
-                      <PaginationItem className="disabled">
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                          tabIndex="-1"
-                        >
-                          <i className="fas fa-angle-left" />
-                          <span className="sr-only">Previous</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem className="active">
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          1
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          2 <span className="sr-only">(current)</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          3
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <i className="fas fa-angle-right" />
-                          <span className="sr-only">Next</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                    </Pagination>
-                  </nav>
-                </CardFooter>
-              </Card>
-            </div>
-          </Row>
-
-        </Container>
+        <PredictionFucntion/>
 
       </>
     );
--- src/views/dagdi/prediction/TerrainDetails.jsx
+++ src/views/dagdi/prediction/TerrainDetails.jsx
@@ -1,75 +1,79 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import { InputGroup } from 'reactstrap';
+import { useTranslation } from "react-i18next";
+
+function PredictionFunction() {
+
+  const state = {
+    cSelected: []
+  };
+
+ const onRadioBtnClick = (rSelected) => {
+    this.setState({ rSelected });
+  };
+
+  const { t } = useTranslation();
+
+
+  return <container>
+  <h1 className="h4 text-success text-left font-weight-bold mb-4">{t("Land Scape")}</h1>
+          
+          <InputGroup onChange={onRadioBtnClick} horizontal>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Land-Scape" class="custom-control-input" id="Plain" type="radio" />
+              <label class="custom-control-label" for="Plain">{t("Plain")}</label>
+            </div>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Land-Scape" class="custom-control-input" id="Hilly" type="radio" />
+              <label class="custom-control-label" for="Hilly">{t("Hilly")}</label>
+            </div>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Land-Scape" class="custom-control-input" id="Valley" type="radio" />
+              <label class="custom-control-label" for="Valley">{t("Valley")}</label>
+            </div>
+          </InputGroup>
+  
+          <h1 className="h4 text-success text-left font-weight-bold mb-4">{t("Rain")}</h1>
+          <InputGroup onChange={onRadioBtnClick} horizontal>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Rain" class="custom-control-input" id="Less" type="radio" />
+              <label class="custom-control-label" for="Less">{t("Less")}</label>
+            </div>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Rain" class="custom-control-input" id="Normal" type="radio" />
+              <label class="custom-control-label" for="Normal">{t("Normal")}</label>
+            </div>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Rain" class="custom-control-input" id="Heavy" type="radio" />
+              <label class="custom-control-label" for="Heavy">{t("Heavy")}</label>
+            </div>
+          </InputGroup>
+          
+          <h1 className="h4 text-success text-left font-weight-bold mb-4">{t("water")}</h1>
+          <InputGroup onChange={onRadioBtnClick} horizontal>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Water" class="custom-control-input" id="Rare" type="radio" />
+              <label class="custom-control-label" for="Rare">{t("Rare")}</label>
+            </div>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Water" class="custom-control-input" id="Average" type="radio" />
+              <label class="custom-control-label" for="Average">{t("Average")}</label>
+            </div>
+            <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
+              <input name="Water" class="custom-control-input" id="High" type="radio" />
+              <label class="custom-control-label" for="High">{t("High")}</label>
+            </div>
+          </InputGroup>
+  
+        </container>
+}
 
 class TerrainDetails extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { cSelected: [] };
-
-    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-  }
-
-  onRadioBtnClick(rSelected) {
-    this.setState({ rSelected });
-  }
+ 
 
   render() {
     return (
-      <container>
-
-
-
-<h1 className="h4 text-success text-left font-weight-bold mb-4">Land Scape</h1>
-        
-        <InputGroup onChange={this.onChange} horizontal>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Land-Scape" class="custom-control-input" id="Plain" type="radio" />
-            <label class="custom-control-label" for="Plain">Plain</label>
-          </div>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Land-Scape" class="custom-control-input" id="Hilly" type="radio" />
-            <label class="custom-control-label" for="Hilly">Hilly</label>
-          </div>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Land-Scape" class="custom-control-input" id="Valley" type="radio" />
-            <label class="custom-control-label" for="Valley">Valley</label>
-          </div>
-        </InputGroup>
-
-        <h1 className="h4 text-success text-left font-weight-bold mb-4">Rain</h1>
-        <InputGroup onChange={this.onChange} horizontal>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Rain" class="custom-control-input" id="Less" type="radio" />
-            <label class="custom-control-label" for="Less">Less</label>
-          </div>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Rain" class="custom-control-input" id="Normal" type="radio" />
-            <label class="custom-control-label" for="Normal">Normal</label>
-          </div>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Rain" class="custom-control-input" id="Heavy" type="radio" />
-            <label class="custom-control-label" for="Heavy">Heavy</label>
-          </div>
-        </InputGroup>
-        
-        <h1 className="h4 text-success text-left font-weight-bold mb-4">Water</h1>
-        <InputGroup onChange={this.onChange} horizontal>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Water" class="custom-control-input" id="Rare" type="radio" />
-            <label class="custom-control-label" for="Rare">Rare</label>
-          </div>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Water" class="custom-control-input" id="Average" type="radio" />
-            <label class="custom-control-label" for="Average">Average</label>
-          </div>
-          <div class="custom-control mr-lg custom-radio mb-3 col-lg-2">
-            <input name="Water" class="custom-control-input" id="High" type="radio" />
-            <label class="custom-control-label" for="High">High</label>
-          </div>
-        </InputGroup>
-
-      </container>
+      <PredictionFunction/>
     );
   }
 }
